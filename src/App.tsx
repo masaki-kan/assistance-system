c7794866caa679fc239739cdb8996838670f765c
masaki-kan <changsu02han@gmail.com>
2025-07-20 01:58:24 +0900

    更新

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,14 @@
-import React, { FC } from "react";
-
+import { FC } from "react";
 import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
 import TopRouter from "./route/index";
-import store from "./store/index";
 
 function App() {
   return (
     <ChakraProvider>
-      {/* <Provider store={store}> */}
       <BrowserRouter basename="/assistance-system">
         <CommonLayout />
       </BrowserRouter>
-      {/* </Provider> */}
     </ChakraProvider>
   );
 }
